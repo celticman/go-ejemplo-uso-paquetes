029b97e1a52977728ed167e9080cbfd9c981cb07
RPF <rpf@rpf.com>
2021-07-07 15:28:39 +0200

    Cambio nombre fichero

--- uso-paquetes.go
+++ uso-paquetes.go
@@ -5,6 +5,6 @@ import "mis-ejercicios/uso-paquetes/matematicas"
 
 func main() {
   numeros := []float64{1,2,3,4}
-  media := matematicas.media(numeros)
+  media := matematicas.Media(numeros)
   fmt.Println(media)
 }
